fe8f48750cf2831bbf0c6f19662e5d1c0bdad0d4
Anton-Lindauer <primula99@outlook.de>
2026-01-28 19:57:49 +0100

    Fixed a bug when there were no AppImage files in the downloads directory Implemented the functionality to move the AppImage file

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import os
+import sys
+import shutil
 fileList = []
-numOfFile = 0
+fileDestination = "C:/Users/primu/Downloads/test/"
 
+# Function to move the AppImage File to the right directory
+def moveFile(selectedFile, fileDestination):
+    print(f"You choose {fileList[choice]}")
+    shutil.move(selectedFile, fileDestination)
+    print("File moved successfully")
+
+# List every AppImage file
 for file in os.listdir("C:/Users/primu/Downloads/"):
     if file.endswith(".AppImage"):
-        fileList.append(os.path.join("/C:/Users/primu/Downloads/", file))
-        numOfFile += 1
-        print(f"{numOfFile}. {file}")
+        fileList.append(os.path.join("C:/Users/primu/Downloads/", file))
+        print(f"{len(fileList)}. {file}")
 
+# Ask user what to do
 fileListLen = len(fileList)
-if fileListLen >= 2:
-    choice = int(input(f"Select a file(1 - {fileListLen})")) -1
-elif fileListLen == 1:
-    input("You have only one .AppImage file")
+if fileListLen >= 2:    #Execute if there is more than one AppImage file
+    choice = int(input(f"Select a file(1 - {fileListLen})")) - 1
+    selectedFile = fileList[choice]
+    moveFile(selectedFile, fileDestination)
+elif fileListLen == 1:  #Execute if there is one AppImage file
+    print("You have only one .AppImage file")
     choice = 0
-elif fileListLen <= 0:
+    selectedFile = fileList[choice] 
+    fileChoice = input("Enter y to use this file")
+    if fileChoice == "y":
+        moveFile(selectedFile, fileDestination)
+    else:
+        sys.exit()
+elif fileListLen <= 0:  #Execute if there are no AppImage files
     print("No .AppImage file found")
-
-print(f"You choose {fileList[choice]}")
-
-#test
+    sys.exit()
