6b2a08b747271cb9ee85758bb7d157778c1e1ad1
Anton-Lindauer <primula99@outlook.de>
2026-02-01 15:41:48 +0100

    Improved readability a bit

--- main.py
+++ main.py
@@ -27,10 +27,15 @@ def mkExec():
     print("The AppImage file can now be executed")
     mkSymLink()
 
-def mkSymLink(): # Function to create a symLink file, to execute the .AppImage file with a terminal command systemwide
+# Function to create a symLink file, to execute the .AppImage file with a terminal command systemwide on your account
+def mkSymLink(): 
     cmdName = input("Enter the command your want to execute the .AppImage file from the terminal: ")
     os.system(f"ln -s ~/AppImages/{os.path.basename(fileList[choice])}""  ~/.local/bin/"+cmdName)
     print(f"You can now use {cmdName} to execute this program from the terminal")
+    mkStartmenuEntry()
+
+def mkStartmenuEntry():
+    pass
 
 # List every AppImage file
 for file in os.listdir("/home/"+username+"/Downloads"):
@@ -43,21 +48,26 @@ for file in fileList:   # Display every file
     print(f"{fileList.index(file) + 1}. {file}")
 
 # Ask user which file to move
-fileListLen = len(fileList)
-if fileListLen >= 2:    # Execute if there is more than one AppImage file
-    choice = int(input(f"Select a file to move (1 - {fileListLen}): ")) - 1
-    if choice >= 0:
-        moveFile(fileList, fileDest)
-    else:
-        print(f"Please select a file from the list (1 - {fileListLen}): ")
-elif fileListLen == 1:  # Execute if there is only one AppImage file
-    print("This .AppImage file has been found")
-    choice = 0 
-    fileChoice = input("Enter y to move this file: ").lower()
-    if fileChoice == "y":
-        moveFile(fileList, fileDest)
-    else:
-        print("Canceling the operation")
-else:  # Execute if there are no AppImage files
-    print("No .AppImage file has been found")
-    sys.exit()
+def usrSelect():
+    fileListLen = len(fileList)
+    global choice
+    if fileListLen >= 2:    # Execute if there is more than one AppImage file
+        choice = int(input(f"Select a file to move (1 - {fileListLen}): ")) - 1
+        if choice >= 0:
+            moveFile(fileList, fileDest)
+        else:
+            print(f"Please select a file from the list (1 - {fileListLen}): ")
+    elif fileListLen == 1:  # Execute if there is only one AppImage file
+        print("This .AppImage file has been found")
+        choice = 0 
+        fileChoice = input("Enter y to move this file: ").lower()
+        if fileChoice == "y":
+            moveFile(fileList, fileDest)
+        else:
+            print("Canceling the operation")
+    else:  # Execute if there are no AppImage files
+        print("No .AppImage file has been found")
+        sys.exit()
+
+if __name__ == "__main__":
+    usrSelect()
