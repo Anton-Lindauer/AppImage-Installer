9dc9f68688dfe8a9c097ce647816d9a634620820
Anton-Lindauer <primula99@outlook.de>
2026-02-10 16:58:29 +0100

    Added all the GUIs elements

--- Pyside.py
+++ Pyside.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QGroupBox, QRadioButton, QPushButton, QFrame
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QGroupBox, QRadioButton, QPushButton, QFrame, QStackedWidget
 from PySide6.QtCore import Qt
 import sys
 from main import showFiles
@@ -15,6 +15,24 @@ class MainWindow(QMainWindow):
 
         mainLayout = QVBoxLayout(central)   # Layout of the entiry window
 
+        self.stackedWidget = QStackedWidget()
+
+        self.page1 = self.createPage1()
+        self.stackedWidget.addWidget(self.page1)
+
+        self.page2 = self.createPage2()
+        self.stackedWidget.addWidget(self.page2)
+
+        self.page3 = self.createPage3()
+        self.stackedWidget.addWidget(self.page3)
+
+        mainLayout.addWidget(self.stackedWidget)
+
+    def createPage1(self):
+
+        widget = QWidget()
+        page1Layout = QVBoxLayout(widget)
+
         title = QLabel("Select a file to install")   # Title of the current thing the user does
         title.setObjectName("title")
         title.setAlignment(Qt.AlignLeft)
@@ -24,7 +42,7 @@ class MainWindow(QMainWindow):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
         container.setLayout(layout)
-        
+
         fileList = showFiles()
         fileListLen = fileList.__len__
 
@@ -48,16 +66,84 @@ class MainWindow(QMainWindow):
 
         submitButton = QPushButton("Continue")  # Button to continue with the selected options
         submitButton.setObjectName("submitBtn")
+        submitButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
 
-        mainLayout.setContentsMargins(50, 50, 50, 50)
+        page1Layout.setContentsMargins(50, 50, 50, 50)
 
-        # Add the elemts to the window
-        mainLayout.addWidget(title)
-        mainLayout.addWidget(container)
-        mainLayout.addWidget(submitButton)
+            # Add the elemts to the window
+        page1Layout.addWidget(title)
+        page1Layout.addWidget(container)
+        page1Layout.addWidget(submitButton)
 
-        mainLayout.addStretch()
+        page1Layout.addStretch()
 
+        return widget
+        
+    def createPage2(self):
+        widget = QWidget()
+        page2Layout = QVBoxLayout(widget)
+
+        title = QLabel("Select a file to install")   # Title of the current thing the user does
+        title.setObjectName("title")
+        title.setAlignment(Qt.AlignLeft)
+
+        submitButton = QPushButton("Continue")  # Button to continue with the selected options
+        submitButton.setObjectName("submitBtn")
+        submitButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+
+        container = QGroupBox()    # Box for the options for the user
+        layout = QVBoxLayout(container)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        container.setLayout(layout)
+
+        line = QFrame()     # Dividers between the elements in the groupbox
+        line.setFixedHeight(1)
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        line.setObjectName("line")
+
+        programInfo = ["Enter a command to execute the file from the terminal: ",
+                       "Enter the name of the program in the startmenu: ",
+                       "Enter a description for the program: ",
+                       "Enter the categories the program belongs to: "]
+        
+        for info in programInfo:
+            entry = QLabel(info)
+            layout.addWidget(entry)
+            entry.setObjectName("entry")
+            if programInfo.index(info) < 3:
+                spacer = QWidget()
+                spacer.setFixedHeight(2)
+                layout.addWidget(spacer)
+                line = QFrame()     # Dividers between the elements in the groupbox
+                line.setFixedHeight(1)
+                line.setFrameShape(QFrame.HLine)
+                line.setFrameShadow(QFrame.Sunken)
+                line.setObjectName("line")
+                layout.addWidget(line)
+
+        page2Layout.addWidget(title)
+        page2Layout.addWidget(container)
+        page2Layout.addWidget(submitButton)
+
+        page2Layout.addStretch()
+
+        return widget
+    
+    def createPage3(self):
+        widget = QWidget()
+        page3Layout = QVBoxLayout(widget)
+
+        submitButton = QPushButton("Install another program")  # Button to continue with the selected options
+        submitButton.setObjectName("submitBtn")
+        submitButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+
+        page3Layout.addWidget(QLabel("Finished installing the program"))
+        page3Layout.addWidget(submitButton)
+
+        page3Layout.addStretch()
+        return widget
 
 if __name__ == "__main__":
     app = QApplication()
